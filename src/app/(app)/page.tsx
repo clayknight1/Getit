c04d444001af951a1e8e47da273382871941c7f2
Clay Knight <clayknight@rocketmail.com>
2025-11-16 08:15:52 -0800

    initial commit

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,8 @@
+export default function Lists() {
+  return (
+    // <div className={styles.page}>
+    //   <main className={styles.main}></main>
+    // </div>
+    <h1>Hello</h1>
+  );
+}
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Login() {
+  return (
+    <div className={styles.loginWrapper}>
+      <div className={styles.loginBox}>
+        <button>LOGIN</button>
+      </div>
+    </div>
+  );
+}
